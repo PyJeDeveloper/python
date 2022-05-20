d4cb6942ab57992fc8068a898cf05f56bf54e721
PyJeDeveloper <91725930+PyJeDeveloper@users.noreply.github.com>
2022-05-20 14:56:16 +0530

    Create core.py

---
+++ core.py
@@ -0,0 +1,8 @@
+#Bot Token From @BotFather
+bot_token = "5338572545:AAFpFJbWmH0fF8mIsannBN1E9PUJTbuaYu0"
+
+#Admins Telegram ID
+admins = [2131044278]
+
+#MongoDb Url For Databse
+mongo_url = "mongodb+srv://Data1:Data1@cluster0.ymfdb.mongodb.net/?retryWrites=true&w=majority"
